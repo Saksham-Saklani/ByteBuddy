8889bcb51d8bbfcb2afe513547cf3b8480dde5c9
Saksham Saklani <140688953+Saksham-Saklani@users.noreply.github.com>
2024-10-03 01:44:26 +0530

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/context";
+
+const Main = () => {
+
+const { 
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat
+    } = useContext(Context)
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p onClick={()=>newChat()}>ByteBuddy</p>
+        <img src={assets.user_icon}></img>
+      </div>
+
+      <div className="main-container">
+        
+       {
+       !showResult
+       ?<>
+        <div className="greet">
+            <p><span>Hello there!</span></p>
+            <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+            <div onClick={()=> setInput("suggest beautiful places to see on an upcoming road trip")}  className="card">
+                <p>suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} />
+            </div>
+            <div onClick={()=>setInput("Briefly summarize this concept: urban planning")} className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} />
+            </div>
+            <div  onClick={()=>setInput("Brainstorm team bonding activities for our work retreat")} className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} />
+            </div>
+            <div onClick={()=>setInput("Improve the readability of our code")} className="card">
+                <p>Improve the readability of our code</p>
+                <img src={assets.code_icon} />
+            </div>
+        </div>
+       </>
+       :  <div className="result">
+        <div className="result-title">
+            <img src={assets.user_icon} />
+            <p> {recentPrompt} </p>
+        </div>
+        <div className="result-data">
+            <img src={assets.robot} />
+            {
+                loading
+                ?<div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                </div>
+                : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+        </div>
+
+       </div>
+
+       }
+
+        
+        <div className="main-bottom">
+            <div className="search-box">
+                <input onChange={(e) => setInput(e.target.value)}  value={input} type="text" placeholder="Enter a prompt here" onKeyDown={(e)=>{if(e.key==='Enter'){onSent();}}} />
+                <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                   {input ? <img  onClick={() => onSent()} src={assets.send_icon} alt="" /> :null}
+                </div>
+                
+            </div>
+            <p className="bottom-info">
+            ByteBuddy may display inaccurate info, including about people, so double-check its responses. 
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
